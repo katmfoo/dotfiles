3e1fdebbbe3926f27344c1a09296b7d001ba347b
katmfoo <katmfoo@gmail.com>
2018-03-28 22:18:46 -0400

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -1,6 +1,6 @@
 # bash.sh, sets up my bash environment
 # author: Patrick Richeal
-# last modified: 3/18/2018
+# last modified: 3/28/2018
 # source: https://github.com/pricheal/dotfiles
 
 # ==================================================
@@ -13,9 +13,6 @@ rm ~/.bash_profile
 # copy new .bash_profile
 mv .bash_profile ~/.bash_profile
 
-# install base16-shell
-git clone https://github.com/chriskempson/base16-shell.git ~/.config/base16-shell
-
 # ==================================================
 # vim
 # ==================================================
@@ -26,21 +23,6 @@ rm -rf ~/.vim
 
 # copy new .vimrc
 mv .vimrc ~/.vimrc
-
-# install base16-vim
-git clone https://github.com/chriskempson/base16-vim.git ~/.vim/colors/base16/
-cp ~/.vim/colors/base16/colors/*.vim ~/.vim/colors/
-rm -rf ~/.vim/colors/base16/
-
-# install vim-airline and themes
-git clone https://github.com/vim-airline/vim-airline ~/.vim/pack/dist/start/vim-airline
-git clone https://github.com/vim-airline/vim-airline-themes ~/.vim/pack/dist/start/vim-airline-themes
-
-# install nerdtree
-git clone https://github.com/scrooloose/nerdtree.git ~/.vim/pack/dist/start/nerdtree
-
-# install vim-fugitive
-git clone https://github.com/tpope/vim-fugitive.git ~/.vim/pack/dist/start/vim-fugitive
 
 # ==================================================
 # self destruct
