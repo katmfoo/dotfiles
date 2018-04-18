3e94912f9c8a436827e595daab4151ce708ab4fe
katmfoo <katmfoo@gmail.com>
2018-04-18 13:23:53 -0400

    Update mac.sh

--- mac.sh
+++ mac.sh
@@ -66,6 +66,9 @@ brew cask install sublime-text
 # install package control
 curl -o '~/Library/Application Support/Sublime Text 3/Installed Packages/Package Control.sublime-package' https://packagecontrol.io/Package%20Control.sublime-package
 
+# move list of installed sublime packages to correct place
+cp 'Package Control.sublime-settings' '~/Library/Application Support/Sublime Text 3/Packages/User/Package Control.sublime-settings'
+
 # ==================================================
 # git
 # ==================================================
