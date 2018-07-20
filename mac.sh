4eb3fc59165c4f10a397aa335eedec01737695b3
katmfoo <katmfoo@gmail.com>
2018-07-19 22:41:19 -0400

    Update mac.sh

--- mac.sh
+++ mac.sh
@@ -11,9 +11,8 @@
 #     - Set mouse/trackpad tracking speed
 #     - Enable dark mode (System preferences, general)
 #     - Put installed apps onto second page of Launchpad
-#     - Remove unused apps from dock and add installed apps (order is Finder, Calendar, Notes, Reminders, Photos, Messages, Dashlane, Google Chrome, Sublime Text, iTerm, System Preferences)
+#     - Remove unused apps from dock and add installed apps (order is Finder, Calendar, Notes, Reminders, Photos, Messages, Dashlane, Google Chrome, iTerm, System Preferences)
 #     - Remove unnecessary items from Finder sidebar (in Preferences of Finder)
-#     - Add Google account under internet accounts
 #     - Install Microsoft Office from Microsoft account
 #     - Install ClearPass OnGuard
 
