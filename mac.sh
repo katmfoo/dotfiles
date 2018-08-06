e5369bd8e7b12d4bb069fd852c77641ff9cbda35
katmfoo <katmfoo@gmail.com>
2018-08-06 11:18:24 -0400

    Update mac.sh

--- mac.sh
+++ mac.sh
@@ -83,6 +83,12 @@ cp fonts/native-bold-italic.ttf ~/Library/Fonts/native-bold-italic.ttf
 git config --global user.email "patrickricheal@gmail.com"
 git config --global user.name "pricheal"
 
+# enable aliases
+git config --global alias.co checkout
+git config --global alias.br branch
+git config --global alias.ci commit
+git config --global alias.st status
+
 # ==================================================
 # bash
 # ==================================================
