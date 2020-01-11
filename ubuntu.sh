4623b2c96ea8368d5df0e83937406e83eca6605b
katmfoo <katmfoo@gmail.com>
2020-01-11 12:24:37 -0500

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -27,7 +27,7 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt-get update
 sudo apt-get -y install docker-ce docker-ce-cli containerd.io
-sudo groupadd docker # todo: remove if not needed
+#sudo groupadd docker # todo: ensure not needed
 sudo usermod -aG docker $USER
 newgrp docker
 
