5dec3f9304e03245ca3cfbb4ee4e322920a5a729
katmfoo <katmfoo@gmail.com>
2020-01-11 17:48:37 -0500

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -53,8 +53,6 @@ brew install neofetch
 # misc
 # ==================================================
 
-touch ~/installs_finished
-
 # download base16
 git clone https://github.com/chriskempson/base16-shell.git ~/.config/base16-shell
 
