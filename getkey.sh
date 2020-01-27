b3088a69c4ae2e996baa39999127e3faafd68a9e
katmfoo <katmfoo@gmail.com>
2020-01-27 03:12:58 +0000

    Make ssh setup work on macos and ubuntu

--- getkey.sh
+++ getkey.sh
@@ -1,5 +1,16 @@
+#!/bin/bash
+
+# getkey.sh, script to retrieve private ssh key, should be sourced on linux to
+# start working in the current shell
+# source: https://github.com/pricheal/dotfiles
+
 eval $(op signin my patrickricheal@gmail.com)
 op get document $(op list documents | jq -r '.[] | select(.overview.title == "SSH private key") | .uuid') > ~/.ssh/id_rsa
 chmod 600 ~/.ssh/id_rsa
 eval $(ssh-agent -s)
-ssh-add -K
+
+if [ "$(uname -s)" == "Darwin" ]; then
+    ssh-add -K
+elif [ "$(uname -s)" == "Linux" ]; then
+    ssh-add
+fi
