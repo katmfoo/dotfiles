4087a157fa058e28c1a1b21b543ebd45fbf33820
katmfoo <katmfoo@gmail.com>
2020-01-29 23:36:32 +0000

    Make getkey.sh work with keychain on linux

--- getkey.sh
+++ getkey.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 
-# getkey.sh, script to retrieve private ssh key, should be sourced on linux to
-# start working in the current shell
+# getkey.sh, script to retrieve private ssh key
 # source: https://github.com/pricheal/dotfiles
 
 eval $(op signin my patrickricheal@gmail.com)
 op get document $(op list documents | jq -r '.[] | select(.overview.title == "SSH private key") | .uuid') > ~/.ssh/id_rsa
 chmod 600 ~/.ssh/id_rsa
 
-# ssh-add -K stores private key passphrase in keychain on macos
 if [ "$(uname -s)" == "Darwin" ]; then
     eval $(ssh-agent -s)
     ssh-add -K
 elif [ "$(uname -s)" == "Linux" ]; then
-    
+    keychain ~/.ssh/id_rsa    
 fi
