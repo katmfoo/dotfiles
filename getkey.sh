c090977c08d3be30b17b5ddafcc0208cea66b14a
katmfoo <katmfoo@gmail.com>
2020-01-29 18:29:15 -0500

    Update getkey.sh

--- getkey.sh
+++ getkey.sh
@@ -7,11 +7,11 @@
 eval $(op signin my patrickricheal@gmail.com)
 op get document $(op list documents | jq -r '.[] | select(.overview.title == "SSH private key") | .uuid') > ~/.ssh/id_rsa
 chmod 600 ~/.ssh/id_rsa
-eval $(ssh-agent -s)
 
 # ssh-add -K stores private key passphrase in keychain on macos
 if [ "$(uname -s)" == "Darwin" ]; then
+    eval $(ssh-agent -s)
     ssh-add -K
 elif [ "$(uname -s)" == "Linux" ]; then
-    ssh-add
+    
 fi
