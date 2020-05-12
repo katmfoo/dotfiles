e1e51c9eed37b2a29137afe214a00841c745116f
katmfoo <katmfoo@gmail.com>
2020-05-11 21:59:30 -0400

    Update getkey.sh

--- getkey.sh
+++ getkey.sh
@@ -7,9 +7,4 @@ eval $(op signin my patrickricheal@gmail.com)
 op get document $(op list documents | jq -r '.[] | select(.overview.title == "SSH private key") | .uuid') > ~/.ssh/id_rsa
 chmod 600 ~/.ssh/id_rsa
 
-if [ "$(uname -s)" == "Darwin" ]; then
-    eval $(ssh-agent -s)
-    ssh-add -K
-elif [ "$(uname -s)" == "Linux" ]; then
-    keychain ~/.ssh/id_rsa    
-fi
+# TODO: add the ssh key to the agent or something
