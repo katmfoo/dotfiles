d8ae5607d53d8fe3ce4fdde8aa476009962083e7
katmfoo <katmfoo@gmail.com>
2020-05-12 11:34:43 -0400

    Update and rename getkey.sh to getkeys.sh

--- getkeys.sh
+++ getkeys.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-# getkey.sh, script to retrieve private ssh key
+# getkey.sh, script to retrieve private keys
 # source: https://github.com/pricheal/dotfiles
 
 eval $(op signin my patrickricheal@gmail.com)
+
+# get ssh private key
 op get document $(op list documents | jq -r '.[] | select(.overview.title == "SSH private key") | .uuid') > ~/.ssh/id_rsa
 chmod 600 ~/.ssh/id_rsa
 
-# TODO: add the ssh key to the agent or something
+# get gpg private key
+op get document $(op list documents | jq -r '.[] | select(.overview.title == "GPG private key") | .uuid') > ~/gpg-private.key
+gpg --import ~/gpg-private.key
