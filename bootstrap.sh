7d997025bf246f181e1d3d8d11e95f3e7c7e0785
katmfoo <katmfoo@gmail.com>
2020-05-11 21:50:53 -0400

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,26 +9,6 @@ elif [ "$(uname -s)" == "Linux" ]; then
     source ~/dotfiles/ubuntu.sh
 fi
 
-# install docker sync
-brew install ruby
-gem install docker-sync
-
-# install dev tools
-brew install tmux
-brew install git
-brew install vim
-brew install less
-brew install httpie
-brew install jq
-brew install --HEAD mosh # head needed for certain color stuff to work
-
-# install misc stuff
-brew install neofetch
-
-# install completion stuff
-brew install bash-completion
-brew install docker-completion
-sudo curl -L https://raw.githubusercontent.com/Bash-it/bash-it/master/completion/available/tmux.completion.bash -o $(brew --prefix)/etc/bash_completion.d/tmux
 
 # install base16 shell
 git clone https://github.com/chriskempson/base16-shell.git ~/.config/base16-shell
@@ -39,7 +19,6 @@ ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
 ln -sf ~/dotfiles/.profile ~/.profile
-ln -sf ~/dotfiles/ssh-config ~/.ssh/config
 ln -sf ~/dotfiles/id_rsa.pub ~/.ssh/id_rsa.pub
 
 # generate key bindings for less
