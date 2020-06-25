cad86df249e7b252129b05c4c53e6a41e04c4c83
katmfoo <katmfoo@gmail.com>
2020-06-25 11:11:17 -0400

    Update getkeys.sh script

--- getkeys.sh
+++ getkeys.sh
@@ -7,11 +7,10 @@
 eval $(op signin my patrickricheal@gmail.com)
 
 # get ssh private key
-mkdir -p ~/.ssh
 op get document $(op list documents | jq -r '.[] | select(.overview.title == "SSH private key") | .uuid') > ~/.ssh/id_rsa
 chmod 600 ~/.ssh/id_rsa
 
 # get gpg private key
-op get document $(op list documents | jq -r '.[] | select(.overview.title == "GPG private key") | .uuid') > ~/gpg-private.key
-gpg --import ~/gpg-private.key
-rm ~/gpg-private.key
+op get document $(op list documents | jq -r '.[] | select(.overview.title == "GPG private key") | .uuid') > ~/.gnupg/gpg-private.key
+gpg --import ~/.gnupg/gpg-private.key
+rm ~/.gnupg/gpg-private.key
