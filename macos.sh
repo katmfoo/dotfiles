e401e6a0a7f1791cefed04478d26afdd37392d74
kat <katmfoo@gmail.com>
2025-08-27 17:06:41 -0400

    Update macos.sh

--- macos.sh
+++ macos.sh
@@ -3,13 +3,37 @@
 # macos.sh, loose notes for macos system install
 # source: https://github.com/katmfoo/dotfiles
 
-# install homebrew
-# install iterm2 (default terminal doesn't support truecolor), configure it
-#   to pull settings from .config/iterm2 and save edits automatically
-# install docker
-# install jetbrains mono
+# set trackpad "tracking speed" to fastest
+# set keyboard "key repeat rate" to fastest
+# set keyboard "delay until repeat" to shortest
+# set finder sidebar to only desired directories
+# set dock to left side of screen and to auto hide
+# turn off "show suggested and recent apps in dock"
+
+# install google chrome, set as default web browser
+# install 1password
+# install spotify
+# install obsidian
+# install discord
+# install iterm2 (default terminal app doesn't support truecolor)
 # install neovide
-# install vscode, need it for gui step debugging
+# install docker desktop
+# install jetbrains mono
+# install datagrip
+# install vscode
+# install slack
+# install zoom
+# install photoshop
+# install xcode
+# install android studio
+# install karabiner elements (config in dotfiles)
+# install amphetamine (keep screen awake)
+# install monitorcontrol (control dell monitor via mac hotkeys)
+# install mos (otherwise non app mouse scrolling is terrible)
+
+# setup dock as desired
+
+# install homebrew
 
 # bash
 brew install bash
@@ -34,7 +58,32 @@ yadm clone --no-bootstrap https://github.com/katmfoo/dotfiles.git
 
 # make ~/dev folder
 
+# configure iterm2 to pull settings from .config/iterm2 and save edits
+# automatically
+
 # setup prettier
 # run `npm install --save-dev prettier @prettier/plugin-php` inside `~/dev`
 # run `npm install -g @fsouza/prettierd`
+
+
+
+
+
+
+
+
+
+
+
+# KAT TODO: add ~/.config/karabiner/karabiner.json to dotfiles
+# KAT TODO: does everything with caps key feel okay? if not, maybe i need to copy the
+# KAT TODO: get rid of stuff from top right bar?
+# KAT TODO: do final organization of dock
+# KAT TODO: install jetbrains mono
+# number settings from old mac as well?
+
+
+
+
+# setup mailbox repo and open in obsidian
 
